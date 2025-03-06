28874338d50cf2cfd8f323da2dbbb28aae25e297
TheRABU <mdfazlerabbi114@gmail.com>
2025-03-06 23:30:41 +0600

    created slider and testimonial section as well as updated the nav links

---
+++ js/index.js
@@ -0,0 +1,27 @@
+const slider_images = [
+  "../images/slider/right-side-img1.png",
+  "../images/slider/right-side-img2.jpg",
+  "../images/slider/right-side-img3.jpg",
+];
+
+const changeTab = (index) => {
+  document.getElementById("tab-image").src = slider_images[index];
+  //remove avtive from the node
+  document
+    .querySelectorAll(".tab-item")
+    .forEach((tab) => tab.classList.remove("active"));
+  // add active
+  document.querySelectorAll(".tab-item")[index].classList.add("active");
+};
+
+window.changeTab = changeTab;
+
+/// TESTIMONIAL LOAD MORE BUTTON
+
+const loadMore = () => {
+  document.querySelectorAll(".testimonial.hidden").forEach((testimonial) => {
+    testimonial.classList.remove("hidden");
+  });
+
+  document.querySelector(".load-more-btn").style.display = "none";
+};
